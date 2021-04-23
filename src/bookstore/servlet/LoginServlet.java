df3db2864de12f19d694de56156ac5ddd1ca2826
Jack hou <256595662@qq.com>
2021-04-23 08:35:57 +0800

    JavaWeb在校课程学习

---
+++ src/bookstore/servlet/LoginServlet.java
@@ -0,0 +1,27 @@
+package bookstore.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//注解进行配置
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        System.out.println("hello");
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        System.out.println("get");
+    }
+
+}
---
+++ src/bookstore/util/JDBCUtil.java
@@ -0,0 +1,84 @@
+package bookstore.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+	
+	public static Connection getConn(){
+		
+		Connection conn=null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String url="jdbc:mysql://localhost:3306/bookstore?useUnicode=true&&characterEncoding=utf8";
+		String user="root";
+		String password="123456";
+		try {
+			conn=DriverManager.getConnection(url,user,password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		return conn;
+		
+	}
+	public static void release(ResultSet rs,PreparedStatement ps,Connection conn){
+		
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(ps!=null){
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+public static void release(PreparedStatement ps,Connection conn){
+		
+		
+		if(ps!=null){
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
